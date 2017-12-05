550a444afb65d32ab96537e1d26eb859e87c809d
Lior Belinsky <liorbe@wix.com>
2017-12-05 23:31:40 +0200

    work with new wix-ui-theme api

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ToggleSwitch from 'wix-ui-tpa/ToggleSwitch';
 import Button from 'wix-ui-tpa/Button';
+import {TpaStylesProvider} from 'wix-ui-tpa';
+import Wix from 'Wix';
 
-const buttonFallbackBindings = {
-  colors: {
-    backgroundColor: 'color_8',
-    color: 'color-1'
-  },
-  fonts: {
-    fonts: 'Body-M'
-  }
-};
+// const buttonFallbackBindings = {
+//   colors: {
+//     backgroundColor: 'color_8',
+//     color: 'color-1'
+//   },
+//   fonts: {
+//     fonts: 'Body-M'
+//   }
+// };
 
 const buttonWixBindings = {
   fonts: '_btnFont',
@@ -25,16 +27,18 @@ const toggleSwitchWixBindings = {
 class Widget extends React.Component {
   render() {
     return (
-      <div className="wix-style-sample">
-        <h2 className="sample-element sample-title">TPA Dummy App</h2>
-        <div style={{width: '300px'}}>
-          <h3 style={{marginBottom: '10px'}}>wix-style-react Button</h3>
-          <Button wixBindings={buttonWixBindings} fallbackBindings={buttonFallbackBindings}>Hello</Button><br/><br/>
-          <ToggleSwitch wixBindings={toggleSwitchWixBindings}/>
-          <h3>Regular button</h3>
-          <button className="sample-element sample-button">Click Me</button><br/><br/><br/><br/>
+      <TpaStylesProvider Wix={Wix}>
+        <div className="wix-style-sample">
+          <h2 className="sample-element sample-title">TPA Dummy App</h2>
+          <div style={{width: '300px'}}>
+            <h3 style={{marginBottom: '10px'}}>wix-style-react Button</h3>
+            <Button wixBindings={buttonWixBindings}>Hello</Button><br/><br/>
+            <ToggleSwitch wixBindings={toggleSwitchWixBindings}>Hello</ToggleSwitch><br/><br/>
+            <h3>Regular button</h3>
+            <button className="sample-element sample-button">Click Me</button><br/><br/><br/><br/>
+          </div>
         </div>
-      </div>
+      </TpaStylesProvider>
     );
   }
 }
